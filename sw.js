ff3298df69a6c58202923bdc57045f187dab7ce2
nacho73 <exceed73@gmail.com>
2026-02-18 17:22:25 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'binaural-v11';
+const CACHE_NAME = 'binaural-v12';
 const assets = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', e => {
